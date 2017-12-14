654a6682973bff073f21c7f4a1b6187e7eadfcda
Vlamingm <31851861+Vlamingm@users.noreply.github.com>
2017-12-14 13:56:30 +0100

    ResetDNS en create reverse lookup zone

---
+++ 201-windows-domain-controller/pdcscript.ps1
@@ -0,0 +1,15 @@
+Set-DnsClientServerAddress -interfacealias "ethernet 1" -resetserveraddresses
+Set-DnsClientServerAddress -interfacealias "ethernet 2" -resetserveraddresses
+Set-DnsClientServerAddress -interfacealias "ethernet 3" -resetserveraddresses
+
+# work out the name of the reverse zone
+$ip = $args[0]
+$ipbits = $ip.Split('.')
+$zone = "$($ipbits[2]).$($ipbits[1]).$($ipbits[0]).in-addr.arpa." 
+dnscmd.exe /ZoneAdd $zone /DsPrimary
+dnscmd.exe /Config $zone /AllowUpdate 1
+if ($LastExitCode -ne 0)
+{
+    "exit code configuring reverse zone ($args[0]) was non-zero ($LastExitCode), bailing..."
+    exit $LastExitCode
+}
